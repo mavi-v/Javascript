674076598dadc60b8f625274686bbb94d7510acc
Mavi <101255967+mavi-v@users.noreply.github.com>
2024-07-05 19:03:31 +0300

    Add files via upload

---
+++ Pop ups/index.js
@@ -0,0 +1,12 @@
+console.log('hello');
+console.log('I like Pizza!')
+
+window.alert('This is an alert!');
+window.alert('I like pizza!');
+
+document.getElementById("myH1").textContent = 'Hello';
+document.getElementById("myP").textContent = 'I like Pizza!';
+
+// This is a comment
+/* */
+
